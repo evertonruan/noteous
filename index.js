ff784516799ba6bb7cf2f7e88d0cec87866567d2
Éverton Ruan <evertonr0798@gmail.com>
2023-02-20 11:49:27 -0300

    noteous 1.5 pwa

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-//SERVICE WORKER ///////
-window.addEventListener('load', () => {
-  registerSW()
-})
-
-async function registerSW() {
-  if ('serviceWorker' in navigator) {
-    try {
-      await navigator.serviceWorker.register('./sw.js')
-    } catch (e) {
-      console.log('Service Worker Registration failed')
-    }
-  }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
 }
 
 // ELEMENTOS ///////
--- sw.js
+++ sw.js
@@ -1,59 +1,62 @@
-const cacheName = 'noteousCache'
+self.addEventListener('install', e => {
+  console.log('[Service Worker] Install')
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName)
+      console.log('[Service Worker] Caching all: app shell and content')
+      await cache.addAll(contentToCache)
+    })()
+  )
+})
 
-this.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/index.js',
-        '/style.css',
-        '/orblendEngine.js',
-        '/about.html',
-        '/about.js',
-        '/manifest.json',
-        '/img/logo-icon-144.png',
-        '/img/logo-icon.png',
-        '/img/cupcake logo.png',
-        '/img/preview.png',
-        '/img/favicon.png',
-        '/img/social-chain.png'
-      ])
+const cacheName = 'noteousCache'
+const contentToCache = [
+  '/',
+  'index.html',
+  'index.js',
+  'style.css',
+  'orblendEngine.js',
+  'about.html',
+  'about.js',
+  'manifest.json',
+  '/img/logo-icon-144.png',
+  '/img/logo-icon.png',
+  '/img/cupcake logo.png',
+  '/img/preview.png',
+  '/img/favicon.png',
+  '/img/social-chain.png'
+]
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request)
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
+      if (r) {
+        return r
+      }
+      const response = await fetch(e.request)
+      const cache = await caches.open(cacheName)
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`)
+      cache.put(e.request, response.clone())
+      return response
+    })()
+  )
+})
+
+//clears the cache
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key === cacheName) {
+            return
+          }
+
+          return caches.delete(key)
+        })
+      )
     })
   )
-  return self.skipWaiting()
 })
-
-self.addEventListener('activate', e => {
-  self.clients.claim()
-})
-
-self.addEventListener('fetch', async e => {
-  const req = e.request
-  const url = new URL(req.url)
-
-  if (url.login === location.origin) {
-    e.respondWith(cacheFirst(req))
-  } else {
-    e.respondWith(networkAndCache(req))
-  }
-})
-
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName)
-  const cached = await cache.match(req)
-
-  return cached || fetch(req)
-}
-
-async function networkAndCache(req) {
-  const cache = await caches.open(cacheName)
-  try {
-    const refresh = await fetch(req)
-    await cache.put(req, refresh.clone())
-    return refresh
-  } catch (e) {
-    const cached = await cache.match(req)
-    return cached
-  }
-}
