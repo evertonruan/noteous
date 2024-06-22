599623e706d0734a43e18f5db958deae32dabc92
Éverton Ruan <evertonr0798@gmail.com>
2024-06-22 10:29:17 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,16 @@
-self.addEventListener('install', e => {
-  console.log('[Service Worker] Install')
-  e.waitUntil(
-    (async () => {
-      const cache = await caches.open(cacheName)
-      console.log('[Service Worker] Caching all: app shell and content')
-      await cache.addAll(contentToCache)
-    })()
-  )
-})
+const noteousResources = ['/', 'index.html', 'index.js', 'about.html', 'about.js', 'style.css', 'reset.css', 'policies.json', 'orblendEngine.js', '/img/cupcake-logo.png', '/img/social-chain.png']
 
-const cacheName = 'noteousCache'
-const contentToCache = [
-  '/',
-  'index.html',
-  'index.js',
-  'style.css',
-  'orblendEngine.js',
-  'about.html',
-  'about.js',
-  'manifest.json',
-  '/img/logo-icon-512.png',
-  '/img/cupcake logo.png',
-  '/img/preview.png',
-  '/img/favicon.png',
-  '/img/social-chain.png'
-]
+//INSTALLATION
+
+
+self.addEventListener("install", event => {
+   event.waitUntil(
+      caches.open("noteous-caches")
+      .then(cache => {
+         return cache.addAll(noteousResources);
+      })
+   )
+})
 
 //ACTIVATION
 //Cache cleaning
@@ -47,22 +33,6 @@ self.addEventListener('activate', e => {
 
 //FETCHING
 //Connect to serve to see update, then go to cache
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-        const networkFetch = fetch(event.request).then(response => {
-          // update the cache with a clone of the network response
-          const responseClone = response.clone()
-          caches.open(url.searchParams.get('name')).then(cache => {
-            cache.put(event.request, responseClone)
-          })
-          return response
-        }).catch(function (reason) {
-          console.error('ServiceWorker fetch failed: ', reason)
-        })
-        // prioritize cached response over network
-        return cachedResponse || networkFetch
-      }
-    )
-  )
-})
+self.addEventListener("fetch", event => {
+  console.log(`URL requested: ${event.request.url}`);
+});
