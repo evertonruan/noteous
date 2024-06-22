e8e89f10fcabadbbc016c4364e2b9f7ad081a7ab
Éverton Ruan <evertonr0798@gmail.com>
2024-06-22 10:42:59 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
 //INSTALLATION
 self.addEventListener("install", event => {
-  event.waitUntil(
-    (async () => {
-      const cache = await caches.open(cacheName)
-      await cache.addAll(noteousResources)
-    })()
-  )
+  try {
+    event.waitUntil(
+      (async () => {
+        const cache = await caches.open(cacheName)
+        await cache.addAll(noteousResources)
+      })()
+    )
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 const noteousResources = ['/', 'index.html', 'index.js', 'about.html', 'about.js', 'style.css', 'reset.css', 'policies.json', 'orblendEngine.js', '/img/cupcake-logo.png', '/img/social-chain.png']
