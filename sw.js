109a2520288d3c57da7c6ace7032ab0550dc71d8
Éverton Ruan <evertonr0798@gmail.com>
2024-06-22 10:51:50 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -42,6 +42,22 @@ self.addEventListener('activate', e => {
 
 //FETCHING
 //Connect to serve to see update, then go to cache
-self.addEventListener("fetch", event => {
-  console.log(`URL requested: ${event.request.url}`);
-});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+        const networkFetch = fetch(event.request).then(response => {
+          // update the cache with a clone of the network response
+          const responseClone = response.clone()
+          caches.open(url.searchParams.get('name')).then(cache => {
+            cache.put(event.request, responseClone)
+          })
+          return response
+        }).catch(function (reason) {
+          console.error('ServiceWorker fetch failed: ', reason)
+        })
+        // prioritize cached response over network
+        return cachedResponse || networkFetch
+      }
+    )
+  )
+})
