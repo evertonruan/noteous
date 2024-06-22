380bbc975eac276913ac0b83c5dfd7e55a0b44fc
Éverton Ruan <evertonr0798@gmail.com>
2024-06-22 09:44:41 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ self.addEventListener('activate', e => {
 //Connect to serve to see update, then go to cache
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
+    cache.match(event.request).then(cachedResponse => {
         const networkFetch = fetch(event.request).then(response => {
           // update the cache with a clone of the network response
           const responseClone = response.clone()
