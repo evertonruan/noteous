ad2aeb3950806930db50cda07b197bde949284ab
Éverton Ruan <evertonr0798@gmail.com>
2024-07-14 19:03:05 -0300

    Add files via upload

--- debug.js
+++ debug.js
@@ -3,10 +3,35 @@ let noteousSettings = JSON.parse(localStorage.getItem('noteous-settings'))
 
 let body = document.querySelector('body')
 
+body.append(document.createTextNode(`Bem-vindo à página de diagnóstico do noteous`))
+
+body.append(document.createElement('br'))
+body.append(document.createElement('br'))
+
+body.append(document.createElement('br'))
+
+body.append(document.createTextNode(`• CONFIGURAÇÕES ARMAZENADAS`))
+
+body.append(document.createElement('br'))
+body.append(document.createElement('br'))
+
 for (let prop in noteousSettings) {
     body.append(document.createTextNode(`${prop}: ${noteousSettings[prop]}`))
     body.append(document.createElement('br'))
 }
+
+body.append(document.createElement('br'))
+body.append(document.createElement('br'))
+body.append(document.createElement('br'))
+
+body.append(document.createTextNode(`• SOLUÇÃO DE PROBLEMAS`))
+body.append(document.createElement('br'))
+body.append(document.createElement('br'))
+
+body.append(document.createTextNode(`noteous não atualiza: um problema pode estar ocorrendo no mecanismo do Service Worker. Clique no botão abaixo para reinstalar. Suas notas não serão afetadas.`))
+
+body.append(document.createElement('br'))
+body.append(document.createElement('br'))
 
 let buttonUnregisterServiceWorker = document.createElement('button')
 buttonUnregisterServiceWorker.setAttribute('onclick','unRegisterServiceWorker()')
@@ -14,19 +39,40 @@ buttonUnregisterServiceWorker.innerText = 'Remover Service Worker'
 body.append(buttonUnregisterServiceWorker)
 
 function unRegisterServiceWorker() {
-    alert('ok')
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations()
+    buttonUnregisterServiceWorker.removeAttribute('onclick')
+    buttonUnregisterServiceWorker.innerText = '🔄️ Removendo Service Worker'
+    
+    setTimeout(() => {
+        if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistrations()
         .then(function(registrations) {
             for (let registration of registrations) {
                 registration.unregister();
             }
         });
 }
+        buttonUnregisterServiceWorker.innerText = '🔄️ Instalando Service Worker'
+        setTimeout(() => {
+            if ('serviceWorker' in navigator) {
+                navigator.serviceWorker.register('sw.js')
+              }
+            buttonUnregisterServiceWorker.innerText = '✅ Service Worker reinstalado'
+            
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-  }
+            body.append(document.createElement('br'))
+            let notice = document.createElement('p')
+            notice.innerHTML = `Agora, siga estes passos para verificar uma atualização: <br>
+            1 - Feche o noteous. Aguarde alguns instantes e abra-o novamente <br>
+            2 - Recarregue a página algumas vezes <br>
+            3 - Caso ainda não atualize, desinstale e instale novamente. Sua notas não serão afetadas se você fizer isso`
+            body.append(notice)
+            body.append(document.createTextNode(``))
+        }, 4000);
+    }, 1000);
+    
+
+
+
 
 }
 
