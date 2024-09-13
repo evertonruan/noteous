8029453069cd57cf68c4ceba4db7695e1cc2cac5
Éverton Ruan <evertonr0798@gmail.com>
2024-09-13 00:33:35 -0300

    noteous preview 1.6.2

--- debug.js
+++ debug.js
@@ -34,6 +34,78 @@ body.append(document.createTextNode(`Versão atual: 1.62. Versão instalada ${no
 
 br()
 
+// Obtém todos os caches disponíveis
+caches.keys().then((cacheNames) => {
+  cacheNames.forEach((cacheName) => {
+    caches.open(cacheName).then((cache) => {
+      cache.keys().then((cachedRequests) => {
+        console.log(`Recursos em cache no ${cacheName}:`);
+        cachedRequests.forEach((request) => {
+          console.log(request.url);
+        });
+      });
+    });
+  });
+});
+
+
+function unRegisterServiceWorker() {
+  buttonUnregisterServiceWorker.removeAttribute('onclick')
+  buttonUnregisterServiceWorker.innerText = '🔄️ Removendo Service Worker'
+  
+  setTimeout(() => {
+      if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations()
+      .then(function(registrations) {
+          for (let registration of registrations) {
+              registration.unregister();
+          }
+      });
+    }
+    buttonUnregisterServiceWorker.innerText = '🔄️ Limpando o cache'
+    setTimeout(() => {
+      caches.keys().then((cacheNames) => {
+        for (const name of cacheNames) {
+          caches.delete(name);
+        }
+      });
+
+      noteousSettings.debug = 'SW'
+      localStorage.setItem('noteous-settings', JSON.stringify(noteousSettings))
+      window.location.reload()
+    }, 2000);
+  }, 2000);
+}
+
+//Ao recarregar a página, verifica se o SW está sendo reinstalado
+if (noteousSettings.debug == 'SW') {
+  reinstallServiceWorker()
+}
+
+
+function reinstallServiceWorker() {
+  buttonUnregisterServiceWorker.removeAttribute('onclick')
+  buttonUnregisterServiceWorker.innerText = '🔄️ Instalando Service Worker'
+  setTimeout(() => {
+      if ('serviceWorker' in navigator) {
+          navigator.serviceWorker.register('sw.js')
+          }
+      buttonUnregisterServiceWorker.innerText = '✅ Service Worker reinstalado'
+
+      noteousSettings.debug = ''
+      localStorage.setItem('noteous-settings', JSON.stringify(noteousSettings))
+
+      body.append(document.createElement('br'))
+      let notice = document.createElement('p')
+      notice.innerHTML = `Agora, siga estes passos para verificar uma atualização: <br>
+      1 - Feche o noteous. Aguarde alguns instantes e abra-o novamente <br>
+      2 - Recarregue a página algumas vezes <br>
+      3 - Caso ainda não atualize, desinstale e instale novamente. Sua notas não serão afetadas se você fizer isso`
+      body.append(notice)
+      body.append(document.createTextNode(``))
+  }, 4000);
+}
+
 if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker.getRegistration().then(function(registration) {
@@ -41,80 +113,7 @@ if ('serviceWorker' in navigator) {
     body.append(document.createTextNode(`Se a versão atual for diferente da versão instalada, um problema pode estar ocorrendo no mecanismo do Service Worker. Clique no botão abaixo para reinstalar. Suas notas não serão afetadas.`))
 
   br()
-  br()
-
-  // Obtém todos os caches disponíveis
-  caches.keys().then((cacheNames) => {
-    cacheNames.forEach((cacheName) => {
-      caches.open(cacheName).then((cache) => {
-        cache.keys().then((cachedRequests) => {
-          console.log(`Recursos em cache no ${cacheName}:`);
-          cachedRequests.forEach((request) => {
-            console.log(request.url);
-          });
-        });
-      });
-    });
-  });
-  
-
-
-function unRegisterServiceWorker() {
-    buttonUnregisterServiceWorker.removeAttribute('onclick')
-    buttonUnregisterServiceWorker.innerText = '🔄️ Removendo Service Worker'
-    
-    setTimeout(() => {
-        if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.getRegistrations()
-        .then(function(registrations) {
-            for (let registration of registrations) {
-                registration.unregister();
-            }
-        });
-      }
-      buttonUnregisterServiceWorker.innerText = '🔄️ Limpando o cache'
-      setTimeout(() => {
-        caches.keys().then((cacheNames) => {
-          for (const name of cacheNames) {
-            caches.delete(name);
-          }
-        });
-
-        noteousSettings.debug = 'SW'
-        localStorage.setItem('noteous-settings', JSON.stringify(noteousSettings))
-        window.location.reload()
-      }, 2000);
-    }, 2000);
-}
-
-//Ao recarregar a página, verifica se o SW está sendo reinstalado
-if (noteousSettings.debug == 'SW') {
-    reinstallServiceWorker()
-}
-
-
-function reinstallServiceWorker() {
-    buttonUnregisterServiceWorker.removeAttribute('onclick')
-    buttonUnregisterServiceWorker.innerText = '🔄️ Instalando Service Worker'
-    setTimeout(() => {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('sw.js')
-            }
-        buttonUnregisterServiceWorker.innerText = '✅ Service Worker reinstalado'
-
-        noteousSettings.debug = ''
-        localStorage.setItem('noteous-settings', JSON.stringify(noteousSettings))
-
-        body.append(document.createElement('br'))
-        let notice = document.createElement('p')
-        notice.innerHTML = `Agora, siga estes passos para verificar uma atualização: <br>
-        1 - Feche o noteous. Aguarde alguns instantes e abra-o novamente <br>
-        2 - Recarregue a página algumas vezes <br>
-        3 - Caso ainda não atualize, desinstale e instale novamente. Sua notas não serão afetadas se você fizer isso`
-        body.append(notice)
-        body.append(document.createTextNode(``))
-    }, 4000);
-}
+  br()  
 
   let buttonUnregisterServiceWorker = document.createElement('button')
   buttonUnregisterServiceWorker.setAttribute('onclick','unRegisterServiceWorker()')
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-//noteous SW version = 240912-5
+//noteous SW version = 240912-6
 
 /*
 When the user accepts the terms, the Service Worker is installed and adds resources to the cache.
