576b0b2b2a7c2167e578fc7fa78625368ce058f7
Éverton Ruan <evertonr0798@gmail.com>
2025-08-07 12:39:48 -0300

    noteous preview 1.7.003 atualiza variável de versão

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ let readNotesList = document.querySelector('#read-notes')
 
 // VARIÁVEIS IMPORTANTES /////////////////////////////////////
 
-let currentVersion = 1.63
+let currentVersion = 1.70
 let noteIdEdit //usada para confirmar qual nota está sendo editada
 let editMode = false
 let tabIndexCounter = 10
