51387eb195bc51991ef13e6a1ed4f769d68c8f8d
Éverton Ruan <evertonr0798@gmail.com>
2026-02-22 20:49:32 -0300

    preview 2.0: update version number and label

--- index.js
+++ index.js
@@ -134,11 +134,7 @@ function navLink() {
 ////
 
 function renderNoteousVersionLabel() {
-  if (noteousVersion.toString().length == 3) {
-    noteousVersionLabel.innerHTML = `<span>noteous preview</span> ${noteousVersion}`
-  } else if (noteousVersion.toString().length == 4) {
-    noteousVersionLabel.innerHTML = `<span>noteous preview</span> ${noteousVersion.toString().slice(0,3)}.${noteousVersion.toString().slice(3)}`
-  }
+  noteousVersionLabel.innerHTML = `<span>noteous preview</span> 2ª Geração`
 }
 renderNoteousVersionLabel()
 
